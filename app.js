55201e60b9d93e890ce6aaf57d59fee6a53dde85
Nnadozie Okeke <n.e.okeke@outlook.com>
2020-07-28 22:35:26 +0100

    buggy challlenge 2 example solution

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 const express = require('express');
 const path = require('path');
+const textObj = require(path.join(__dirname, '/public/text.json'));
+
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/text', (req, res) => {
+
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/text.json'));
 });
 
+
+const deleteNodes = (parent, type) => {
+
+    const keys = Object.keys(parent);
+    if (keys.includes(type)) {
+        delete parent[type];
+    }
+    else {
+        keys.forEach(key => {
+            deleteNodes(parent[`${key}`], type);
+        });
+    }
+
+}
+
 app.get('/jp/ja', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/text.json'));
+    let jaObj = {};
+    if (true) {
+        let clone = JSON.parse(JSON.stringify(textObj));
+        deleteNodes(clone, "en");
+        jaObj = clone;
+    }
+    res.send(jaObj);
+
 });
 
 app.get('/ng/en', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/text.json'));
+    let enObj = {};
+    if (true) {
+        let clone = JSON.parse(JSON.stringify(textObj));
+        deleteNodes(clone, "ja");
+        enObj = clone;
+    }
+    res.send(enObj);
 });
 
 app.listen(port, () =>{
