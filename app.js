631b6e17d812e40334eeaa8852d96d6594fc6d91
Nnadozie Okeke <n.e.okeke@outlook.com>
2020-08-08 20:33:16 +0100

    secret

--- app.js
+++ app.js
@@ -5,41 +5,41 @@ const selectLanguage = require('./utilities/selectLanguage');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const url = "mongodb+srv://admin:57VpzmLLKHAGdJD@uberclone.1g6uu.mongodb.net/uberCloneDb?retryWrites=true&w=majority";
+const dbName = 'uberCloneDb';
+
 
 
 app.use(express.json());
-app.post('/:collection', (req, res) => {
-
-    const url = 'mongodb://localhost:27017';
-    const dbName = 'uberCloneDb';
+app.put('/:collection', (req, res) => {
     const payload = req.body;
     const { collection } = req.params;
 
     (async function mongo() {
-        let client;
+        let client = new MongoClient(url, { useNewUrlParser: true });
         try {
             client = MongoClient.connect(url);
             const db = (await client).db(dbName);
-            const response = await db.collection(collection).insertOne(payload);
-            res.json(response)
+            const col = await db.collection(collection.toString());
+            await col.deleteMany();
+            const response = await col.insertOne(payload);
+            res.status(201).json(response)
         } catch (error) {
             res.send(error);
-            console.log(error);
         }
         (await client).close();
     })();
 });
 
-app.get('/', (req, res) => {
-    const url = 'mongodb://localhost:27017';
-    const dbName = 'uberCloneDb';
+app.get('/:collection', (req, res) => {
+    const {collection} = req.params;
 
     (async function mongo() {
-        let client;
+        let client = new MongoClient(url, { useNewUrlParser: true });
         try {
             client = MongoClient.connect(url);
             const db = (await client).db(dbName);
-            const col = await db.collection('default');
+            const col = await db.collection(collection.toString());
             const text = await col.findOne()
             res.send(text);
         } catch (error) {
@@ -50,16 +50,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:collection/:language', (req, res) => {
-    const url = 'mongodb://localhost:27017';
-    const dbName = 'uberCloneDb';
-    const { country, language } = req.params;
+    const { collection, language } = req.params;
 
     (async function mongo() {
-        let client;
+        let client = new MongoClient(url, { useNewUrlParser: true });
         try {
             client = MongoClient.connect(url);
             const db = (await client).db(dbName);
-            const col = await db.collection(country);
+            const col = await db.collection(collection.toString());
             const text = await col.findOne()
             res.send(selectLanguage(text, language))
         } catch (error) {
